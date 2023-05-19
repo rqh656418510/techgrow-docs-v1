d02500a6f5100d434738f94cf32a938332052f11
rqh656418510 <656418510@qq.com>
2023-05-19 13:55:45 +0800

    :sparkles: Update configs

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,8 +3,11 @@ module.exports = {
   // 基础配置
   base: '/v1/',
   head: [
-    ['meta', { name: 'application-name', content: 'TechGrow 开放平台' }],
-    ['meta', { name: 'apple-mobile-web-app-title', content: 'TechGrow 开放平台' }],
+	['meta', { 'http-equiv': 'expires', content: '0' }],
+	['meta', { 'http-equiv': 'pragram', content: 'no-cache' }],
+	['meta', { 'http-equiv': 'cache-control', content: 'no-cache, no-store, must-revalidate' }],
+    ['meta', { name: 'application-name', content: 'TechGrow 官方文档' }],
+    ['meta', { name: 'apple-mobile-web-app-title', content: 'TechGrow 官方文档' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '30x30', href: `/images/icons/favicon-30x30.png` }]
   ],
